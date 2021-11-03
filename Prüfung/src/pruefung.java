07ed604c4642df28598342f963799488af8f7a59
jurek-nky <Jurek.nit@gmail.com>
2021-11-03 14:30:25 +0100

    aufgabe 2-4 added

--- Prüfung/src/pruefung.java
+++ Prüfung/src/pruefung.java
@@ -1,6 +1,34 @@
 public class pruefung {
     public static void main(String[] args) {
-        System.out.println(quersumme(42543));
+        System.out.println("alle quersummen = 15 :\n");
+        for (int i = 0; i < 1000; i++) {                        // aufgabe 2
+            if (quersumme(i) == 15) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("\n\n\n vielfaches von sieben :\n");
+        for (int i = 0; i < 1000; i++) {                        // aufgabe 3
+            if (quersumme(i) % 7 == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("\n\n\nHaeufigkeitsverteilung 1-1000 :");
+        int most = 0;
+        int num = 0;
+        for (int i = 0; i < 27; i++) {
+            int count = 0;
+            for (int j = 0; j < 1000; j++) {
+                if (quersumme(j) == i) {
+                    count++;
+                }
+            }
+            if (count > most) {
+                most = count;
+                num = i;
+            }
+        }
+        System.out.println(String.format("Die quersumme %s kommt mit %s mal am haeufigsten vor.",num,most));
+
     }
 
     static int quersumme(int number) {
